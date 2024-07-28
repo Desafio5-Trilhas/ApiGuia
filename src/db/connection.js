e27a08e3a4fd79f5e36e86617d4851dfebdd6c58
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2024-07-28 00:34:51 -0300

    fix: conexao com banco dialectModule

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 const config = require('./config.js');
+const pg = require('pg');
 
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
@@ -7,6 +8,7 @@ const dbConfig = config[env];
 const sequelize = new Sequelize(dbConfig.url, {
   dialect: dbConfig.dialect,
   logging: dbConfig.logging,
+  dialectModule: pg,
 });
 
 module.exports = sequelize;
