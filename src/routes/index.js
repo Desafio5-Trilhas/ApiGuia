9f36271e165a57c331efce1223353b9bd8ce87b8
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2024-07-28 21:31:21 -0300

    fix: inclusa rota de missao no index das rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usuario = require('./usuarioRoute.js');
 const destino = require('./destinoRoute.js');
+const missao = require('./missaoRoute.js');
 
 const routes = (app) => {
   app
@@ -11,6 +12,7 @@ const routes = (app) => {
 
   app.use(express.json(), usuario);
   app.use(express.json(), destino);
+  app.use(express.json(), missao);
 };
 
 module.exports = routes;
